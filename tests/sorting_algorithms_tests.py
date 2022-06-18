b48ad3201dce2c46b8e8c6f9a11037f0532bd88a
Puthearath Aim <u7227681@anu.edu.augit config --global user.email u7227681@anu.edu.augit config --global user.email u7227681@anu.edu.au>
2022-06-18 21:59:46 +1000

    added more tests

--- tests/sorting_algorithms_tests.py
+++ tests/sorting_algorithms_tests.py
@@ -5,20 +5,27 @@ from src.sorting_algorithms import bubble_sort
 
 
 class SortingAlgorithmTests(unittest.TestCase):
+
     @staticmethod
-    def __generate_random_array(arr_length):
+    def gen_random_array(arr_length):
         random_arr = list()
         for element in range(arr_length):
             random_arr += [random.randint(0, arr_length * 2)]
         return random_arr
 
-    def test_bubble_sort(self):
-        for arrays in range(2 ** 4):  # tests on 2 ** 4 arrays
-            arr_length = random.randint(2 ** 4, 2 ** 8)
-            unsorted_arr = self.__generate_random_array(arr_length)
-            expected_arr = sorted(unsorted_arr)
-            actual_arr = bubble_sort(unsorted_arr)
-            self.assertListEqual(expected_arr, actual_arr)
+    # TEST BUBBLE SORT
+
+    def test_bubble_sort_empty(self):
+        unsorted_arr = []
+        expected_arr = []
+        actual_arr = bubble_sort(unsorted_arr)
+        self.assertListEqual(expected_arr, actual_arr)
+
+    def test_bubble_sort_random(self):
+        unsorted_arr = self.gen_random_array(random.randint(2 ** 4, 2 ** 8))
+        expected_arr = sorted(unsorted_arr)
+        actual_arr = bubble_sort(unsorted_arr)
+        self.assertListEqual(expected_arr, actual_arr)
 
 
 if __name__ == '__main__':
